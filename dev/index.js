51077703b6ab3936bdd0aa41f8fcd6984904ca61
Steve Swanson <steve.e.swanson@gmail.com>
2017-08-26 16:28:43 -0500

    * basic setup for the HipChatter class

--- dev/index.js
+++ dev/index.js
@@ -1,3 +1,46 @@
-"use strict";
+'use strict';
 
-(function () {})();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
+  var HipChatter = function () {
+    function HipChatter(el) {
+      _classCallCheck(this, HipChatter);
+
+      this.el = el;
+      this.input = el.querySelector('input[type=text]');
+      this.button = el.querySelector('button');
+      this.output = el.querySelector('.output');
+
+      this.attachEvents();
+    }
+
+    _createClass(HipChatter, [{
+      key: 'attachEvents',
+      value: function attachEvents() {
+        var _this = this;
+
+        this.button.addEventListener('click', function (e) {
+
+          console.log(_this.input.value);
+        });
+      }
+    }, {
+      key: 'parseValue',
+      value: function parseValue() {
+        var mentionExp = / /;
+        var emoticonExp = / /;
+        var urlExp = / /;
+      }
+    }]);
+
+    return HipChatter;
+  }();
+
+  ;
+
+  var chatterEl = document.querySelector('.chatter');
+  var hipChatter = new HipChatter(chatterEl);
+})();
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,35 @@
 (()=>{
 
+  class HipChatter {
+    constructor(el){
+      this.el = el;
+      this.input = el.querySelector('input[type=text]');
+      this.button = el.querySelector('button');
+      this.output = el.querySelector('.output');
 
+      this.attachEvents();
+    }
+
+
+    attachEvents(){
+      this.button.addEventListener('click', e => {
+
+        console.log(this.input.value);
+      });
+    };
+
+
+    parseValue(){
+      let mentionExp = / /;
+      let emoticonExp = / /;
+      let urlExp = / /;
+    };
+
+
+  };
+
+  let chatterEl = document.querySelector('.chatter');
+  let hipChatter = new HipChatter(chatterEl);
 
 })();
 
