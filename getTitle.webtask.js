fdede1e61e39a3bd90d4a676e2ef250d413cb384
zero0Halo <steve.e.swanson@gmail.com>
2017-09-03 17:07:34 -0500

    * added some comments * added some comments * added some comments * added some comments * added some comments * added some comments * added some comments * added some comments

--- getTitle.webtask.js
+++ getTitle.webtask.js
@@ -1,31 +1,37 @@
 var request = require('request');
 
-var getTitle = function(context, cb){
-  var titleExp = /<title\>(.*)<\/title>/;
-  var url = context.body.url || false;
-  var title = false;
-  var returnObj = {
-    title: '',
-    url
+/**
+ * [getTitle Gets the title of a website based on a submitted url]
+ * @param  {[obj]}   context [Object provided by webtask.io that holds 'context' information about the request]
+ * @param  {Function} cb      [description]
+ * @return {[obj]}           [Strictly speaking, this doesn't have a return. However the callback will send a response back containing the returnObj variable]
+ */
+  var getTitle = function(context, cb){
+    var titleExp = /<title\>(.*)<\/title>/;
+    var url = context.body.url || false;
+    var title = false;
+    var returnObj = {
+      title: '',
+      url
+    };
+
+    if(url){
+      request.get(url, {}, function(err, res, body){
+        try {
+          returnObj.title = titleExp.exec(res.body)[1];
+        } catch(err){
+          returnObj.title = 'Invalid URL';
+          returnObj.error = err.message;
+        }
+
+        cb(null, returnObj);
+      });
+    }
+
+    else {
+      cb(null, 'No Url Provided')
+    }
+
   };
-
-  if(url){
-    request.get(url, {}, function(err, res, body){
-      try {
-        returnObj.title = titleExp.exec(res.body)[1];
-      } catch(err){
-        returnObj.title = 'Invalid URL';
-        returnObj.error = err.message;
-      }
-
-      cb(null, returnObj);
-    });
-  }
-
-  else {
-    cb(null, 'No Url Provided')
-  }
-
-};
 
 module.exports = getTitle;
